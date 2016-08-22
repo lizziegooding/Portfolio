350603b5db2b70a983679a0d9003562a8720a123
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-08-22 14:17:17 -0400

    Finished skills section, circles formatted correctly

--- js/skills.js
+++ js/skills.js
@@ -5,6 +5,35 @@
   }
 
   Skills.prototype.toHtml = function() {
+    this.level1 = this.level2 = this.level3 = this.level4 = this.level5 = 'black';
+    if (this.level < 5) {
+      this.level5 = 'white';
+    }
+    if (this.level < 4) {
+      this.level4 = 'white';
+    }
+    if (this.level < 3) {
+      this.level3 = 'white';
+    }
+    if (this.level < 2) {
+      this.level2 = 'white';
+    }
+    // switch(this.level) {
+    // default:
+    //   this.level1 = this.level2 = this.level3 = this.level4 = this.level5 = 'black';
+    // case 4:
+    //   this.level5 = 'white';
+    //   break;
+    // case 3:
+    //   this.level4 = this.level5 = 'white';
+    //   break;
+    // case 2:
+    //   this.level5 = this.level4 = this.level3 = 'white';
+    //   break;
+    // case 1:
+    //   this.level5 = this.level4 = this.level3 = this.level2 = 'white';
+    //   break;
+    // }
     var template = Handlebars.compile($('#skills-template').html());
     return template(this);
   };
